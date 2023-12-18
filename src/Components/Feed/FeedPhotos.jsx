01760a234e98fd22015cb719716496c1d264b6b1
Meani Freitas <meani.sf@gmail.com>
2023-12-18 15:53:08 -0300

    Photo components

--- src/Components/Feed/FeedPhotos.jsx
+++ src/Components/Feed/FeedPhotos.jsx
@@ -7,7 +7,7 @@ import Loading from '../Helper/Loading';
 import styles from './FeedPhotos.module.css';
 
 
-const FeedPhotos = () => {
+const FeedPhotos = ({setModalPhoto}) => {
   const { data, loading, error, request } = useFetch();
 
   React.useEffect(() => {
@@ -30,7 +30,7 @@ const FeedPhotos = () => {
     return (
       <ul className={`${styles.feed} animeLeft`}>
         {data.map(photo => {
-          return <FeedPhotosItem key={photo.id} photo={photo}/>
+          return <FeedPhotosItem key={photo.id} photo={photo} setModalPhoto={setModalPhoto} />
         })}
       </ul>
     );
