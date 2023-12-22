1d58257f5c5ad4adc312eb3b663ec69eee35ab73
Meani Freitas <meani.sf@gmail.com>
2023-12-22 16:02:14 -0300

    Footer component

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import styles from './Footer.module.css';
+import DogsFooter from '../Assets/dogs-footer.svg?react';
 
 const Footer = () => {
   return (
-    <footer className={styles.footer}>Footer</footer>
+    <footer className={styles.footer}>
+      <DogsFooter />
+      <p>Copyright Dogs</p>
+    </footer>
   );
 }
 
